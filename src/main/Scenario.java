d5b0557be159660b91432274676bc5e6fdca9c2e
kiefferbp <kiefferbp@gmail.com>
2017-02-05 00:46:04 -0600

    various: refactor testing

--- src/main/Scenario.java
+++ src/main/Scenario.java
@@ -1,5 +1,7 @@
 package main;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 /**
@@ -11,6 +13,82 @@ public class Scenario {
     public Card dealerCard;
     public boolean isPlayerSoft;
     public boolean isPair;
+
+    public static List<Scenario> getAll() {
+        final List<Scenario> result = new ArrayList<>();
+
+        for (Card dealerCard : Card.values()) {
+            final ScenarioBuilder builder = new ScenarioBuilder()
+                    .setDealerCard(dealerCard)
+                    .setPairFlag(false)
+                    .setSoftFlag(false);
+
+            // hard 5-21 (non-pairs)
+            for (int playerValue = 5; playerValue <= 21; playerValue++) {
+                builder.setPlayerValue(playerValue);
+                result.add(builder.build());
+            }
+
+            // soft 13-21 (non-pairs)
+            builder.setSoftFlag(true);
+            for (int playerValue = 13; playerValue <= 21; playerValue++) {
+                builder.setPlayerValue(playerValue);
+                result.add(builder.build());
+            }
+
+            // 2-2, 3-3, ..., 10-10 pairs
+            builder.setSoftFlag(false);
+            builder.setPairFlag(true);
+            for (int cardValue = 2; cardValue <= 10; cardValue++) {
+                builder.setPlayerValue(cardValue);
+                result.add(builder.build());
+            }
+
+            // add pair of Aces
+            result.add(builder.setSoftFlag(true).setPlayerValue(12).build());
+        }
+
+        return result;
+    }
+
+    public static List<Scenario> getAllWithDealerCardValue(int targetValue) {
+        final List<Scenario> allScenarios = getAll();
+        final List<Scenario> result = new ArrayList<>();
+
+        for (Scenario scenario : allScenarios) {
+            if (scenario.dealerCard.getValue() == targetValue) {
+                result.add(scenario);
+            }
+        }
+
+        return result;
+    }
+
+    public static List<Scenario> getAllSoftHands() {
+        final List<Scenario> allScenarios = getAll();
+        final List<Scenario> result = new ArrayList<>();
+
+        for (Scenario scenario : allScenarios) {
+            if (scenario.isPlayerSoft) {
+                result.add(scenario);
+            }
+        }
+
+        return result;
+    }
+
+    public static List<Scenario> getAllPairs() {
+        final List<Scenario> allScenarios = getAll();
+        final List<Scenario> result = new ArrayList<>();
+
+        for (Scenario scenario : allScenarios) {
+            if (scenario.isPair) {
+                result.add(scenario);
+            }
+        }
+
+        return result;
+    }
 
     @Override
     public boolean equals(Object o) {
--- src/test/DeciderTest.java
+++ src/test/DeciderTest.java
@@ -2,6 +2,12 @@ package test;
 
 import main.*;
 import org.junit.Test;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by Brian on 2/4/2017.
@@ -9,6 +15,31 @@ import org.junit.Test;
 public class DeciderTest {
     private static final double ERROR_MARGIN = 0.002;
     private static final Decider d = new Decider(400, 1.0);
+
+    @Parameterized.Parameters
+    public static Iterable<?> data() {
+        // source: http://wizardofodds.com/games/blackjack/appendix/1/
+        final Map<Scenario, Map<Decision, Double>> paramMap = new HashMap<>();
+
+        // hard 21 V A: standing = 0.922194, hitting = -1, doubling = -2, splitting = Integer.MIN_VALUE
+        final Scenario h21vAMapScenario = new ScenarioBuilder()
+                .setPlayerValue(21)
+                .setDealerCard(Card.ACE)
+                .setSoftFlag(false)
+                .setPairFlag(false)
+                .build();
+        final Map<Decision, Double> h21vAMap = new HashMap<>();
+        h21vAMap.put(Decision.STAND, 0.922194);
+        h21vAMap.put(Decision.HIT, -1.0);
+        h21vAMap.put(Decision.DOUBLE, -2.0);
+        h21vAMap.put(Decision.SPLIT, (double) Integer.MIN_VALUE);
+        paramMap.put(h21vAMapScenario, h21vAMap);
+
+        return paramMap.entrySet();
+    }
+
+    @Parameterized.Parameter
+    private Map<Scenario, Map<Decision, Double>> param;
 
     private static boolean approximatelyEqual(double a, double b) {
         return (Math.abs(a - b) < ERROR_MARGIN);
@@ -19,33 +50,39 @@ public class DeciderTest {
             org.junit.Assert.assertTrue(bool);
             System.out.println(description + " - passed");
         } catch (AssertionError e) {
-            System.err.println(description + " - failed");
+            System.err.println(description + " - FAILED!");
             throw e;
         }
     }
 
+    private static Pair<Decision, Double> maxOverMap(Map<Decision, Double> map) {
+        Decision bestDecision = null;
+        double bestValue = Integer.MIN_VALUE;
+
+        for (Map.Entry<Decision, Double> entry : map.entrySet()) {
+            final Decision entryDecision = entry.getKey();
+            final double entryValue = entry.getValue();
+
+            if (entryValue > bestValue) {
+                bestDecision = entryDecision;
+                bestValue = entryValue;
+            }
+        }
+
+        return new Pair<>(bestDecision, bestValue);
+    }
+
     @Test
     public void testDecision() throws Exception {
-        final Scenario scenario = new Scenario();
-        scenario.playerValue = 16;
-        scenario.isPlayerSoft = false;
-        scenario.isPair = true;
-        scenario.dealerCard = Card.ACE;
+        for (Map.Entry<Scenario, Map<Decision, Double>> entry : param.entrySet()) {
+            final Scenario scenario = entry.getKey();
+            final Map<Decision, Double> decisionMap = entry.getValue();
+            final Decision bestDecision = maxOverMap(decisionMap).get(Decision.class);
+            final double bestValue = maxOverMap(decisionMap).get(Double.class);
 
-        Pair<Decision, Double> p = d.computeBestScenarioResult(scenario, true);
-        Decision bestDecision = p.get(Decision.class);
-        double bestResult = p.get(Double.class);
-        assertTrue("We should split two 8's vs an Ace", bestDecision.equals(Decision.SPLIT));
-        assertTrue("The expected value of two 8's vs an Ace should be about -0.372535", approximatelyEqual(bestResult, -0.372535));
-
-        scenario.playerValue = 21;
-        scenario.isPlayerSoft = false;
-        scenario.isPair = false;
-        scenario.dealerCard = Card.ACE;
-        p = d.computeBestScenarioResult(scenario, true);
-        bestDecision = p.get(Decision.class);
-        bestResult = p.get(Double.class);
-        assertTrue("We should stand on a hard 21 vs an Ace", bestDecision.equals(Decision.STAND));
-        assertTrue("The expected value of a hard 21 vs an Ace should be about 0.922194", approximatelyEqual(bestResult, 0.922194));
+            final Pair<Decision, Double> p = d.computeBestScenarioResult(scenario, true);
+            assertTrue("We should " + bestDecision + " on " + scenario, bestDecision.equals(p.get(Decision.class)));
+            assertTrue("The expected value of " + scenario + " is about " + bestValue, approximatelyEqual(p.get(Double.class), bestValue));
+        }
     }
 }
